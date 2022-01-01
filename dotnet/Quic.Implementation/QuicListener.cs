ef513f904d5dfcb958466cd1df8da437f4d81e8e
Timon <timonpost@hotmail.nl>
2022-01-01 14:16:27 +0100

    Rename projects and various refactors.

---
+++ dotnet/Quic.Implementation/QuicListener.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Quick.Native;
+using Quick.Native.ApiWrappers;
+using Quick.Native.Events;
+using Quick.Native.Handles;
+
+namespace Quick.Implementation
+{
+    public class QuicListener : Endpoint
+    {
+        private readonly ServerConfig _serverConfig;
+        private readonly Dictionary<int, ConnectionHandle> _connections;
+
+        private SemaphoreSlim _signal = new SemaphoreSlim(0, 1);
+        private int _lastConnection = 0;
+
+
+        public QuicListener(IPEndPoint ipEndpoint)
+        {
+            _serverConfig = new ServerConfig();
+            var result = QuinnApi.create_endpoint(_serverConfig.Handle, out int id, out EndpointHandle handle);
+            
+            if (result.Erroneous())
+                throw new Exception(LastQuinnError.Retrieve().Reason);
+
+            base.Id = id;
+            base.Handle = handle;
+
+            _connections = new Dictionary<int, ConnectionHandle>();
+
+            QuicSocket = new QuickSocket(ipEndpoint);
+            EndpointEvents.NewConnection += OnNewConnection;
+            EndpointEvents.TransmitReady += OnTransmitReady;
+        }
+
+        private void OnTransmitReady(object sender, TransmitEventArgs e)
+        {
+            if (Id == e.Id)
+            {
+                Console.WriteLine("c#; On Transmit: endpoint: {0} dest: {1}, length: {2}", e.Id,
+                    e.TransmitPacket.Destination, e.TransmitPacket.Contents.Length);
+            }
+        }
+
+        private void OnNewConnection(object sender, NewConnectionEventArgs e)
+        {
+            _connections[e.Id] = e.ConnectionHandle;
+            _lastConnection = e.Id;
+            _signal.Release();
+        }
+        
+        public void Recieve()
+        {
+            Console.WriteLine("Receiving...");
+            var buffer = QuicSocket.Receive(out var address);
+            Console.WriteLine("Processing Incoming...");
+            EndpointApi.HandleDatagram(Handle, buffer, address);
+        }
+
+        public async Task<QuicConnection> AcceptIncomingAsync()
+        {
+            Console.WriteLine("Listening...");
+
+            Recieve();
+            await _signal.WaitAsync();
+            _signal.Dispose();
+            _signal = new SemaphoreSlim(0, 1);
+            return new QuicConnection(_connections[_lastConnection], _lastConnection);
+        }
+
+        public void Poll()
+        {
+            foreach (var connection in _connections)
+            {
+                QuinnApi.poll_connection(connection.Value);
+            }
+
+            QuinnApi.poll_endpoint(Handle);
+        }
+    }
+}
---
+++ dotnet/Quic.Native/Events/ConnectionEvents.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Quick.Native.Handles;
+
+namespace Quick.Native.Events
+{
+    public static class ConnectionEvents
+    { 
+        public static void Initialize()
+        {
+            QuinnApi.set_on_connected(OnConnected);
+            QuinnApi.set_on_connection_lost(OnConnectionLost);
+
+            QuinnApi.set_on_datagram_received(OnDatagramReceived);
+      
+            QuinnApi.set_on_stream_opened(OnStreamOpened);
+            QuinnApi.set_on_stream_available(OnStreamAvailable);
+            QuinnApi.set_on_stream_writable(OnStreamWritable);
+            QuinnApi.set_on_stream_readable(OnStreamReadable);
+            QuinnApi.set_on_stream_stopped(OnStreamStopped);
+            QuinnApi.set_on_stream_finished(OnStreamFinished);
+        }
+
+
+        public static event EventHandler<ConnectionIdEventArgs> ConnectionInitialized;
+        public static event EventHandler<ConnectionIdEventArgs> ConnectionLost;
+
+        public static event EventHandler<StreamEventArgs> StreamOpened;
+        public static event EventHandler<StreamEventArgs> StreamAvailable;
+        public static event EventHandler<StreamEventArgs> StreamWritable;
+        public static event EventHandler<StreamEventArgs> StreamReadable;
+        public static event EventHandler<StreamEventArgs> StreamFinished;
+        public static event EventHandler<StreamEventArgs> StreamStopped;
+        public static event EventHandler<ConnectionIdEventArgs> DatagramReceived;
+
+      
+        public static void OnConnected(int connectionId)
+        {
+            Console.WriteLine("C#; OnConnected; Connection ID: {0}", connectionId);
+            ConnectionInitialized?.Invoke(null, new ConnectionIdEventArgs(connectionId));
+        }
+
+        private static void OnConnectionLost(int connectionId)
+        {
+            Console.WriteLine("C#; OnConnectionLost; Connection ID: {0}", connectionId);
+            ConnectionLost?.Invoke(null, new ConnectionIdEventArgs(connectionId));
+        }
+
+        private static void OnStreamOpened(int connectionId, byte streamType)
+        {
+            Console.WriteLine("C#; OnStreamOpened; Connection ID: {0} Stream Type: {1}", connectionId, streamType);
+            StreamOpened?.Invoke(null, new StreamEventArgs(connectionId, streamType));
+        }
+
+        private static void OnStreamAvailable(int connectionId, byte streamType)
+        {
+            Console.WriteLine("C#; OnStreamOpened; Connection ID: {0} Stream Type: {1}", connectionId, streamType);
+            StreamAvailable?.Invoke(null, new StreamEventArgs(connectionId, streamType));
+        }
+        
+        private static void OnStreamWritable(int connectionId, long streamId)
+        {
+            Console.WriteLine("C#; OnStreamWritable; Connection ID: {0} Stream: {1}", connectionId, streamId);
+            StreamWritable?.Invoke(null, new StreamEventArgs(connectionId, streamId));
+        }
+
+        private static void OnStreamReadable(int connectionId, long streamId)
+        {
+            Console.WriteLine("C#; OnStreamReadable; Connection ID: {0} Stream: {1}", connectionId, streamId);
+            StreamReadable?.Invoke(null, new StreamEventArgs(connectionId, streamId));
+        }
+
+        private static void OnStreamStopped(int connectionId, long streamId)
+        {
+            Console.WriteLine("C#; OnStreamStopped; Connection ID: {0} Stream: {1}", connectionId, streamId);
+            StreamStopped?.Invoke(null, new StreamEventArgs(connectionId, streamId));
+        }
+
+        private static void OnStreamFinished(int connectionId, long streamId)
+        {
+            Console.WriteLine("C#; OnStreamFinished; Connection ID: {0} Stream: {1}", connectionId, streamId);
+            StreamFinished?.Invoke(null, new StreamEventArgs(connectionId, streamId));
+        }
+
+        private static void OnDatagramReceived(int connectionId)
+        {
+            Console.WriteLine("C#; OnDatagramReceived; Connection ID: {0}", connectionId);
+            DatagramReceived?.Invoke(null, new ConnectionIdEventArgs(connectionId));
+        }
+    }
+}
